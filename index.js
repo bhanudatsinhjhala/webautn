536829a5c3de191575d233244eef16fe7632c42d
Bhanudatsinh Jhala <jhalabhanudattsinh4090@gmail.com>
2023-03-05 13:13:58 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+var challenge = new Uint8Array(32);
+window.crypto.getRandomValues(challenge);
+
+var userID = "Kosv9fPtkDoh4Oz7Yq/pVgWHS8HhdlCto5cR0aBoVMw=";
+var id = Uint8Array.from(window.atob(userID), (c) => c.charCodeAt(0));
+
+var publicKey = {
+  challenge: challenge,
+
+  rp: {
+    name: "EventPad CORP",
+  },
+
+  user: {
+    id: id,
+    name: "alice@example.com",
+    displayName: "Alice Liddell",
+  },
+
+  pubKeyCredParams: [
+    { type: "public-key", alg: -7 },
+    { type: "public-key", alg: -257 },
+  ],
+};
+
+navigator.credentials
+  .create({ publicKey: publicKey })
+  .then((newCredentialInfo) => {
+    console.log("SUCCESS", newCredentialInfo);
+  })
+  .catch((error) => {
+    console.log("FAIL", error);
+  });
